48af7b06e5e777497f2649102e61554043079f3a
Kyle Coble <kyle@voyantphotonics.com>
2025-09-11 18:33:48 -0400

    Add pre-commit formatting

--- src/egovel_data_capture/launch/recording.launch.py
+++ src/egovel_data_capture/launch/recording.launch.py
@@ -1,16 +1,25 @@
 import os
 from datetime import datetime
 from launch import LaunchDescription
-from launch.actions import  ExecuteProcess
+from launch.actions import ExecuteProcess
 
 
 def generate_launch_description():
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-    
-    return LaunchDescription([
-        ExecuteProcess(
-            cmd=['ros2', 'bag', 'record', '/point_cloud', '/tf_static',
-                 '-o', f'data/bags/lidar_capture_{timestamp}'],
-            output='screen'
-        )
-    ])
+
+    return LaunchDescription(
+        [
+            ExecuteProcess(
+                cmd=[
+                    "ros2",
+                    "bag",
+                    "record",
+                    "/point_cloud",
+                    "/tf_static",
+                    "-o",
+                    f"data/bags/lidar_capture_{timestamp}",
+                ],
+                output="screen",
+            )
+        ]
+    )
