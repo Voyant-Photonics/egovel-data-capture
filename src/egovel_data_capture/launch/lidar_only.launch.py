807b0b09a7d6d5a5368ef96b3199a5be062af6c7
Kyle Coble <kyle@voyantphotonics.com>
2025-09-15 16:16:49 -0400

    Remap topic /point_cloud -> /voyant/point_cloud

--- src/egovel_data_capture/launch/lidar_only.launch.py
+++ src/egovel_data_capture/launch/lidar_only.launch.py
@@ -33,6 +33,9 @@ def generate_launch_description():
         executable="voyant_sensor_node",
         name="voyant_sensor",
         parameters=[voyant_config],
+        remappings=[
+            ("/point_cloud", "/voyant/point_cloud")
+        ],  # TODO: Remove when this is inherent to ROS node
         output="screen",
     )
 
