6c99fd2327e95e02ed12d92e3c100e4e360dc348
Kyle Coble <kyle@voyantphotonics.com>
2025-09-19 13:30:09 -0400

    Apply formatting

--- src/egovel_data_capture/launch/gps.launch.py
+++ src/egovel_data_capture/launch/gps.launch.py
@@ -12,8 +12,7 @@ from launch_ros.actions import SetRemap
 
 def generate_launch_description():
     # Get the path to the ublox_gps package and ntrip_client package
-    ntrip_client_pkg = ament_index_python.get_package_share_directory(
-        'ntrip_client')
+    ntrip_client_pkg = ament_index_python.get_package_share_directory("ntrip_client")
 
     default_config = os.path.join(
         ament_index_python.get_package_share_directory("egovel_data_capture"),
@@ -22,54 +21,59 @@ def generate_launch_description():
         "gps.yaml",
     )
 
-    with open(default_config, 'r') as f:
+    with open(default_config, "r") as f:
         config = yaml.safe_load(f)
 
     # Declare the config file argument
     config_file_arg = DeclareLaunchArgument(
-        'config_file',
+        "config_file",
         default_value=default_config,
-        description='Path to the ublox_gps_node config file'
+        description="Path to the ublox_gps_node config file",
     )
 
     # Include the ublox_gps_node launch file with the config file argument
-    params = config['ublox_gps_node']['ros__parameters']
+    params = config["ublox_gps_node"]["ros__parameters"]
     ublox_launch = ComposableNodeContainer(
-        name='ublox_gps_container',
-        namespace='',
-        package='rclcpp_components',
-        executable='component_container',
+        name="ublox_gps_container",
+        namespace="",
+        package="rclcpp_components",
+        executable="component_container",
         composable_node_descriptions=[
-                ComposableNode(
-                    package='ublox_gps',
-                    plugin='ublox_node::UbloxNode',
-                    name='ublox_gps_node',
-                    parameters=[params]),
+            ComposableNode(
+                package="ublox_gps",
+                plugin="ublox_node::UbloxNode",
+                name="ublox_gps_node",
+                parameters=[params],
+            ),
         ],
-        output='both',
+        output="both",
     )
     # Launch the ntrip client node with the appropriate arguments
-    ntrip_configs = config['ntrip_client_config']['ros__parameters']
+    ntrip_configs = config["ntrip_client_config"]["ros__parameters"]
     ntrip_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([
-            os.path.join(ntrip_client_pkg, 'ntrip_client_launch.py')
-        ]),
+        PythonLaunchDescriptionSource(
+            [os.path.join(ntrip_client_pkg, "ntrip_client_launch.py")]
+        ),
         launch_arguments={
-            'host': ntrip_configs['host'],
-            'port': ntrip_configs['port'],
-            'mountpoint': ntrip_configs['mountpoint'],
-            'username': ntrip_configs['username'],
-            'password': ntrip_configs['password'],
+            "host": ntrip_configs["host"],
+            "port": ntrip_configs["port"],
+            "mountpoint": ntrip_configs["mountpoint"],
+            "username": ntrip_configs["username"],
+            "password": ntrip_configs["password"],
         }.items(),
     )
     # Remap the topics expected by ntrip_client to the ublox node outputs
-    ntrip_with_remaps = GroupAction([
-        SetRemap(src='fix', dst='/ublox_gps_node/fix'),
-        ntrip_launch,
-    ])
+    ntrip_with_remaps = GroupAction(
+        [
+            SetRemap(src="fix", dst="/ublox_gps_node/fix"),
+            ntrip_launch,
+        ]
+    )
 
-    return LaunchDescription([
-        config_file_arg,
-        ublox_launch,
-        ntrip_with_remaps,
-    ])
+    return LaunchDescription(
+        [
+            config_file_arg,
+            ublox_launch,
+            ntrip_with_remaps,
+        ]
+    )
