f4eb888fa999ecc07ee441475629394b576d33ac
kyle-the-voyant <kyle@voyantphotonics.com>
2025-09-22 10:29:20 -0400

    Log GPS data in /gps namespace (#13)

--- src/egovel_data_capture/launch/gps.launch.py
+++ src/egovel_data_capture/launch/gps.launch.py
@@ -31,7 +31,7 @@ def generate_launch_description():
         description="Path to the ublox_gps_node config file",
     )
 
-    # Include the ublox_gps_node launch file with the config file argument
+    # Include the ublox_gps_node launch file with the config file argument and remaps
     params = config["ublox_gps_node"]["ros__parameters"]
     ublox_launch = ComposableNodeContainer(
         name="ublox_gps_container",
@@ -44,11 +44,23 @@ def generate_launch_description():
                 plugin="ublox_node::UbloxNode",
                 name="ublox_gps_node",
                 parameters=[params],
+                # Remap all ublox topics to /gps/ namespace
+                remappings=[
+                    ("/ublox_gps_node/fix", "/gps/fix"),
+                    ("/ublox_gps_node/fix_velocity", "/gps/fix_velocity"),
+                    ("/ublox_gps_node/navpvt", "/gps/navpvt"),
+                    ("navstatus", "/gps/navstatus"),
+                    ("navrelposned", "/gps/navrelposned"),
+                    ("navcov", "/gps/navcov"),
+                    ("nmea", "/gps/nmea"),
+                    ("rtcm", "/gps/rtcm"),
+                ],
             ),
         ],
         output="both",
     )
-    # Launch the ntrip client node with the appropriate arguments
+
+    # Launch the ntrip client node with the appropriate arguments and remaps
     ntrip_configs = config["ntrip_client_config"]["ros__parameters"]
     ntrip_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
@@ -62,10 +74,14 @@ def generate_launch_description():
             "password": ntrip_configs["password"],
         }.items(),
     )
-    # Remap the topics expected by ntrip_client to the ublox node outputs
+
+    # Remap the ntrip client topics to match the new GPS namespace
     ntrip_with_remaps = GroupAction(
         [
-            SetRemap(src="fix", dst="/ublox_gps_node/fix"),
+            # NTRIP client subscribes to fix & nmea and publishes rtcm
+            SetRemap(src="fix", dst="/gps/fix"),
+            SetRemap(src="nmea", dst="/gps/nmea"),
+            SetRemap(src="rtcm", dst="/gps/rtcm"),
             ntrip_launch,
         ]
     )
